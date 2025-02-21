fcafd2fbce472d15c22301788980bef2d5df1f62
Alp Unsal <aunsal3@uwo.ca>
2025-02-21 18:53:42 -0500

    trying to make sound in the glasses

--- my_script.py
+++ my_script.py
@@ -1,1 +1,13 @@
-print('setting up')
+# Testing if i can get sound
+import sounddevice as sd
+import numpy as np
+
+fs = 44100  
+duration = 5
+frequency = 440
+
+t = np.linspace(0, duration, int(fs * duration), endpoint=False)
+audio = 0.5 * np.sin(2 * np.pi * frequency * t)
+
+sd.play(audio, samplerate=fs)
+sd.wait()
